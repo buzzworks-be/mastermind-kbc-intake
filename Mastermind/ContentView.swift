9aa39b67f94c2301bc92f018154c41a9b47135c9
Dennis Litjens <dennis@MacBook-Pro-van-Dennis.local>
2026-03-10 12:08:57 +0100

    Initial project set-up

---
+++ Mastermind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mastermind
+//
+//  Created by Dennis Litjens on 10/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mastermind/MastermindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MastermindApp.swift
+//  Mastermind
+//
+//  Created by Dennis Litjens on 10/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct MastermindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
